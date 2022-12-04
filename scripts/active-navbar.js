559130396435132e27852bf24942aa97e5e71c37
ulugbek <ulugbekyolchimurodov@gmail.com>
2022-12-05 01:58:17 +0500

    Finished About section

--- scripts/active-navbar.js
+++ scripts/active-navbar.js
@@ -1,12 +1,23 @@
 const sections = document.querySelectorAll("section");
 const navbarEle = document.querySelectorAll("#navbar a");
 
+const navbar = document.querySelector("#navbar");
+
+function stcky () {
+	if(pageYOffset > 40) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
+
 window.onscroll = () => {
+	stcky();
 	let current = "";
 
 	sections.forEach((section) => {
 		const sectionTop = section.offsetTop;
-		if (pageYOffset > sectionTop - 60) {
+		if (pageYOffset > sectionTop - 40) {
 			current = section.getAttribute("id");
 		}
 	});
