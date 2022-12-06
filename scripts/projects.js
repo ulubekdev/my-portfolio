b98f2fc8e8d9ac7b19257608e9e6c6a43c238b1c
ulubekdev <ulugbekyolchimurodov@gmail.com>
2022-12-06 20:19:14 +0500

    Finished Projects Section

---
+++ scripts/projects.js
@@ -0,0 +1,27 @@
+const projectsContainer = document.querySelector(".projects_container");
+
+async function renderProjects() {
+	let data = await fetch("../data/projects.json");
+	data = await data.json();
+
+	data.forEach((ele) => {
+		let project = document.createElement("div");
+		project.className = "projects__project";
+		project.setAttribute("pro-id", ele.id);
+		project.innerHTML = `
+            <div class="content">
+                <a href="${ele.url}" target="_blank">
+                <div class="content-overlay"></div>
+                <img class="content-image" src="${ele.image}">
+                <div class="content-details fadeIn-top">
+                    <h3>${ele.name}</h3>
+                    <p>${ele.description}</p>
+                </div>
+                </a>
+            </div>
+        `;
+
+        projectsContainer.append(project);
+	});
+}
+renderProjects();
