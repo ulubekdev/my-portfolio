8658db2a8ad11574cd835649e3dc8450437bde47
Mike Devy <ulugbekyolchimurodov@gmail.com>
2022-12-11 21:13:55 +0500

    Noned display

--- scripts/mobile-navbar.js
+++
@@ -1,8 +0,0 @@
-function hamburger() {
-	let nav = document.getElementById("myTopnav");
-	if (nav.className === "topnav") {
-		nav.className += " responsive";
-	} else {
-		nav.className = "topnav";
-	}
-}
